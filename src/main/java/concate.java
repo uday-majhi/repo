563a25dbddc112de57f747621bde1f283ceeb254
uday-majhi <umajhi1@gmail.com>
2024-05-30 21:55:28 +0545

    added commited file her

---
+++ src/main/java/concate.java
@@ -0,0 +1,8 @@
+public class concate {
+    public static void main(String[] args) {
+        String name = "Uday";
+        String lname = "Majhi";
+        System.out.println(name.concat(lname));
+
+    }
+}
